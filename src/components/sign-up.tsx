1acc74aac012a2da7724fc150dfcbbeef295373b
Ryan Yogan <ryanyogan@gmail.com>
2023-09-14 22:23:09 -0500

    feat // sign up

--- src/components/sign-up.tsx
+++ src/components/sign-up.tsx
@@ -2,27 +2,25 @@ import Link from "next/link";
 import { Icons } from "./icons";
 import UserAuthForm from "./user-auth-form";
 
-export default function SignIn() {
+export default function SignUp() {
   return (
-    <div className="container mx-auto flex flex-col w-full justify-center space-y-6 sm:w-[400px]">
+    <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
       <div className="flex flex-col space-y-2 text-center">
         <Icons.logo className="mx-auto h-6 w-6" />
-        <h1 className="text-2xl font-semibold tracking-tight">Welcome back</h1>
-
+        <h1 className="text-2xl font-semibold tracking-tight">Sign Up</h1>
         <p className="text-sm max-w-xs mx-auto">
           By continuing, you are setting up a Breadit account and agree to our
           User Agreement and Privacy Policy.
         </p>
       </div>
       <UserAuthForm />
-
-      <p className="px-8 text-center text-sm text-zinc-700">
-        New to Breadit?{" "}
+      <p className="px-8 text-center text-sm text-muted-foreground">
+        Already a Breadditor?{" "}
         <Link
-          href="/sign-up"
-          className="hover:text-zinc-800 text-sm underline underline-offset-4"
+          href="/sign-in"
+          className="hover:text-brand text-sm underline underline-offset-4"
         >
-          Sign Up
+          Sign in
         </Link>
       </p>
     </div>
